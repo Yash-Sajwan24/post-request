fc0754d04a85c5d046f2378c9f20df27bcbce6bd
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-04-26 04:28:05 +0530

    post req

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
 
-function App() {
+const App = () => {
+  const [movie, setMovie] = useState("");
+  const [date, setDate] = useState("");
+  const [data, setData] = useState([]);
+
+  const submitHandler = () => {
+    const list ={
+        title: movie, 
+        detail: date,
+    }
+    setData(list);
+    sendData();
+  };
+
+  async function sendData(){
+    const response = await fetch('https://react-http-f2e92-default-rtdb.firebaseio.com//movies.json', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const det = await response.json();
+
+    setMovie("");
+    setDate("");
+    console.log(det);
+  }
+
+  async function fetchHandler(){
+    const response = await fetch('https://react-http-f2e92-default-rtdb.firebaseio.com//movies.json');
+
+    const det = await response.json();
+    
+    const load = [];
+    for (const key in det){
+        load.push({
+            id: key, 
+            title: det[key].title, 
+            detail: det[key].detail,
+        })
+    }
+    setData(load);
+
+  }
+
+  let content=<h1>nothing to show- fetch it</h1>;
+
+  if(data.length > 0){
+    content = data.map((e) => {
+        return (<>
+         <h2>{e.title}</h2>
+        <h2>{e.detail}</h2>
+        </>)
+       
+    });
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <input
+        type="text"
+        value={movie}
+        placeholder="enter the movie name"
+        onChange={(e) => {
+          setMovie(e.target.value);
+        }}
+      ></input>
+      <input
+      value={date}
+        type="text"
+        placeholder="enter the year of the movie"
+        onChange={(e) => {
+          setDate(e.target.value);
+        }}
+      ></input>
+      <button  onClick={submitHandler}>Submit data </button>
+      <button onClick={fetchHandler}> Fetch data</button>
+      {content}
+    </Fragment>
   );
-}
+};
 
 export default App;
